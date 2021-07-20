b469c724ded1e4eff3d27c93e0e65c4c265c2507
Julianeme <juliantve@gmail.com>
2021-07-20 14:22:07 -0500

    print_num| uploading file for firt time

--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,7 @@
 #define BINARY_TREES_H
 
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -33,5 +33,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t max(size_t val_1, size_t val_2);
+void print_num(int n);
 
 #endif
---
+++ print_num.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * print_num - Prints a number
+ *
+ * @n: Number to be printed
+ */
+void print_num(int n)
+{
+	printf("%d\n", n);
+}
