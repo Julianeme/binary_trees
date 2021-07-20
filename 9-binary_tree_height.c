73856363fc7d544157e6c904e7d1aa71c95c4a3a
Julianeme <juliantve@gmail.com>
2021-07-20 14:44:15 -0500

    gramma correction on files 6-8

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
+
+/**
+ * max_val - function that return the biggest of two values
+ * @val_1: first value
+ * @val_2: Second value
+ * Return: the biggest of two values
+ */
+
+size_t max_val(size_t val_1, size_t val_2)
+{
+	if (val_1 >= val_2)
+		return (val_1);
+	return (val_2);
+}
 
 /**
  * binary_tree_height - function that measures the height of a binary tree
