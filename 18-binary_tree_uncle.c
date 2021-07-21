97cb3b35032dd22fcaa4c31667e52b14e58571d5
klich404 <klich4010@gmail.com>
2021-07-21 11:05:50 -0500

    betty correction

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - finds the sibling of a node
+ * binary_tree_uncle - finds the uncle of a node
  * @node: a pointer to the node to find the sibling
  * Return: a pointer to the sibling node or NULL otherwise
  */
