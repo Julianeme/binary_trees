872271bbe66ef2923ddde87c1a307e209c4b4a43
klich404 <klich4010@gmail.com>
2021-07-21 11:03:15 -0500

    task 18 first time and header update

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,11 +8,14 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node && node->parent->parent->left && node->parent->parent->right)
+	if (node && node->parent && node->parent->parent)
 	{
-		if (node->parent == node->parent->parent->left)
-			return (node->parent->parent->right);
-		return (node->parent->parent->left);
+		if (node->parent->parent->left && node->parent->parent->right)
+		{
+			if (node->parent == node->parent->parent->left)
+				return (node->parent->parent->right);
+			return (node->parent->parent->left);
+		}
 	}
 	return (NULL);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -45,5 +45,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 int findADepth(const binary_tree_t *tree);
 int isPerfectRec(const binary_tree_t *tree, int d, int level);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif
